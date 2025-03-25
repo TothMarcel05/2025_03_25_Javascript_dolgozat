336cebeaab5bd887535eea1d7379a14d1bf03d4c
TothMarcel05 <toth.marcel.daniel@diak.szbi-pg.hu>
2025-03-25 08:40:21 +0100

    .

---
+++ js.js
@@ -0,0 +1,31 @@
+//1.
+let person = prompt("Írd be a neved");
+if (person != null) {
+  document.getElementById("demo").innerHTML =
+"Szia " + person + "! Örülök, hogy itt vagy!";
+} 
+
+//2.
+let a = Math.random(1, 10);
+console.log(a)
+
+//3.
+Math.random(1, 6);
+
+//4.
+let b = prompt("Írj be egy számot");
+let c = prompt("Írj be egy másik számot");
+Math.random(b, c);
+
+//5.
+let eletkor = prompt("Írd be az életkorod");
+if (eletkor >= 0 && eletkor<= 6) console.log("gyerek")
+else if (eletkor >= 7 && eletkor<= 18) console.log("iskolás")
+else if (eletkor >= 19 && eletkor<= 60) console.log("dolgozó")
+else console.log("nyugdíjas")
+
+//6.
+let szamelojel =  prompt("Írj be egy számot");
+if (szamelojel > 0) console.log("pozitív")
+else if (szamelojel < 0) console.log("negatív")
+else console.log("nulla")
